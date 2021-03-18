8d270dfc17e663119511c618ba9cd0796ad5e41b
stercoris <dimarodin0@gmail.com>
2021-03-18 11:59:58 +0400

    added routes.tsx

--- App.tsx
+++ App.tsx
@@ -1,32 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { UserInfoProvider } from "./src/ContextProvider/ContextProvider";
+import { Routes } from "./src/Routes";
 
-const client = new ApolloClient({
-  uri: "http://192.168.141.151:4588/graphql",
-  cache: new InMemoryCache(),
-});
-
-export default function App() {
-  return (
-    <ApolloProvider client={client}>
-      <UserInfoProvider>
-        <View style={styles.container}>
-          <Text>Hello World </Text>
-          <StatusBar style="auto" />
-        </View>
-      </UserInfoProvider>
-    </ApolloProvider>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default Routes;
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,17 +1,26 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { React } from "@ungap/global-this";
+import { Text, View } from "react-native";
 import { ChatView } from "./views/Chat";
 
 interface RouteProps {}
 
 const Stack = createStackNavigator();
 
+function HelloWorld() {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
 export const Routes: React.FC<RouteProps> = ({}) => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Chat" component={ChatView} />
+        <Stack.Screen name="Chat" component={HelloWorld} />
       </Stack.Navigator>
     </NavigationContainer>
   );
