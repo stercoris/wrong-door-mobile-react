f69cde46c1485baabd3697d17bf6aec5fc4a3fbb
Leeeeeeeeeeeeeeeeeeeee <dimarodin0@gmail.com>
2021-07-17 18:08:53 +0400

    new pwa manifest and icons

--- app.config.js
+++ app.config.js
@@ -1,28 +1,17 @@
 export default {
-	name: "WrongDoorMobileReact",
-	slug: "WrongDoorMobileReact",
+	name: "Wrong Door",
+	slug: "WD",
 	version: "1.0.0",
 	orientation: "portrait",
+	favicon: "./assets/icon.png",
 	icon: "./assets/icon.png",
 	splash: {
 		image: "./assets/splash.png",
 		resizeMode: "contain",
-		backgroundColor: "#ffffff",
+		backgroundColor: "#000",
 	},
 	updates: {
 		fallbackToCacheTimeout: 0,
 	},
 	assetBundlePatterns: ["**/*"],
-	ios: {
-		supportsTablet: true,
-	},
-	android: {
-		adaptiveIcon: {
-			foregroundImage: "./assets/adaptive-icon.png",
-			backgroundColor: "#FFFFFF",
-		},
-	},
-	web: {
-		favicon: "./assets/favicon.png",
-	},
 };
